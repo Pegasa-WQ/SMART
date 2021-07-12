a7fc202c9cd1ef7518259ecbfa9e4da8c98ca4ec
Pegasa-WQ <sviderek442@gmail.com>
2021-07-12 12:44:25 +0700

    adaptiv

--- AppSon/index.js
+++ AppSon/index.js
@@ -1,14 +1,44 @@
 const swiper = new Swiper('.swiper-container', {
-    slidesPerView: 2.6,
+    slidesPerView: 1,
     slidesPerColumn: 1,
     slidesPerGroup: 1,
     spaceBetween: 30,
     allowTouchMove: true,
     direction: 'horizontal',
     loop: true,
+
+    breakpoints: {
+      420: {
+        slidesPerView: 2,
+        slidesPerColumn: 1,
+        slidesPerGroup: 1,
+        spaceBetween: 30,
+      },
+      767: {
+        slidesPerView: 3,
+        slidesPerColumn: 1,
+        slidesPerGroup: 1,
+        spaceBetween: 30,
+      },
+      1200: {
+        slidesPerView: 2.6,
+        slidesPerColumn: 1,
+        slidesPerGroup: 1,
+        spaceBetween: 30,
+      },
+    },
   
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
     },
+  });
+
+  window.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('#burger').addEventListener('click', function () {
+      document.querySelector('#nav').classList.toggle('is-active')
+    })
+  })
+  document.querySelector('.header__burger').addEventListener('click', (e) => {
+    e.currentTarget.classList.toggle('burger--active');
   });
